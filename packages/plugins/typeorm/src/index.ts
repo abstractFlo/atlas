7957a5cdf8fabddd531916d7a07d1bf3a0e939c0
Florian Uhlrich <f.uhlrich@abstractmedia.de>
2021-08-04 22:13:21 +0200

    WIP: Add plugin-typeorm

--- packages/plugins/typeorm/src/index.ts
+++ packages/plugins/typeorm/src/index.ts
@@ -1,3 +1,6 @@
 import './services/database.service';
 
+export { AutoAdd } from './decorators/auto-add.decorator';
+
+// Re-Export TypeORM to enable import from this plugin
 export * from 'typeorm';
--- packages/plugins/typeorm/src/services/database.service.ts
+++ packages/plugins/typeorm/src/services/database.service.ts
@@ -1,8 +1,28 @@
-import { Init, Singleton, UtilsService } from '@abstractflo/atlas-shared';
-import { createConnection } from 'typeorm';
+import { constructor, getFrameworkMetaData, Init, Singleton, UtilsService } from '@abstractflo/atlas-shared';
+import { ConnectionOptions, createConnection } from 'typeorm';
+import { Internals } from '../internals';
 
 @Singleton
 export class DatabaseService {
+
+  /**
+   * Contains the database connection info
+   *
+   * @type {ConnectionOptions}
+   * @private
+   */
+  private config: ConnectionOptions = {
+    name: 'default',
+    type: process.env.TYPEORM_CONNECTION as any,
+    host: process.env.TYPEORM_HOST,
+    port: Number(process.env.TYPEORM_PORT),
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    logging: process.env.TYPEORM_LOGGING === 'true',
+    synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+    entities: []
+  };
 
   /**
    * Contains if database already conntected
@@ -11,6 +31,15 @@ export class DatabaseService {
    * @private
    */
   private connected: boolean = false;
+
+  @Init(-2)
+  public getReflectEntities(): Promise<void> {
+    return new Promise((resolve) => {
+      const entities = getFrameworkMetaData<constructor<any>[]>(Internals.DATABASE_ENTITIES, this);
+      entities.forEach((entity) => this.config.entities.push(entity));
+      resolve();
+    });
+  }
 
   /**
    * Setup database connection
@@ -22,7 +51,8 @@ export class DatabaseService {
   protected async connect(): Promise<void> {
     if (this.connected) return;
 
-    await createConnection();
+    await createConnection(this.config);
+
     UtilsService.logLoaded('DatabaseService');
     this.connected = true;
   }
