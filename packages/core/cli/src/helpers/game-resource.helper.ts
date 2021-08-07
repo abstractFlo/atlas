bcac76dceffee4271a2e54f858de2517766be09b
Florian Uhlrich <f.uhlrich@abstractmedia.de>
2021-08-07 15:34:39 +0200

    Fix generating resource on new command

--- packages/core/cli/src/helpers/game-resource.helper.ts
+++ packages/core/cli/src/helpers/game-resource.helper.ts
@@ -16,20 +16,20 @@ import {
  * @return {Promise<({file: string, name: string} | {file: string, name: string} | {file: string, name: string} | {file: string, name: string, standalone: boolean} | {file: string, name: string} | {file: string, name: string} | {file: string, name: string} | {file: string, name: string, standalone: boolean} | {file: {isGameResource: boolean, name: string}, name: string} | {file: string, name: string, standalone: boolean})[]>}
  */
 export const gameResourceInstallerFiles = async (resourceName: string, isStandalone: boolean) => ([
-  { name: 'server/components/.gitkeep', file: 'empty' },
-  { name: 'server/services/.gitkeep', file: 'empty' },
+  { name: `${resourceName}/server/components/.gitkeep`, file: 'empty' },
+  { name: `${resourceName}/server/services/.gitkeep`, file: 'empty' },
   {
-    name: 'server/server.module.ts',
+    name: `${resourceName}/server/server.module.ts`,
     file: await renderTemplateFromString(ejsClassTemplate, { className: 'ServerModule', ...moduleClass })
   },
-  { name: 'server/index.ts', file: serverEntryFileTemplate(resourceName), standalone: true },
-  { name: 'client/components/.gitkeep', file: 'empty' },
-  { name: 'client/services/.gitkeep', file: 'empty' },
+  { name: `${resourceName}/server/index.ts`, file: serverEntryFileTemplate(resourceName), standalone: true },
+  { name: `${resourceName}/client/components/.gitkeep`, file: 'empty' },
+  { name: `${resourceName}/client/services/.gitkeep`, file: 'empty' },
   {
-    name: 'client/client.module.ts',
+    name: `${resourceName}/client/client.module.ts`,
     file: await renderTemplateFromString(ejsClassTemplate, { className: 'ClientModule', ...moduleClass })
   },
-  { name: 'client/index.ts', file: clientEntryFileTemplate(resourceName), standalone: true },
-  { name: 'atlas-resource.json', file: atlasResourceJson(resourceName, isStandalone) },
-  { name: 'assets/resource.cfg', file: cfgFromObject(gameResource), standalone: true }
+  { name: `${resourceName}/client/index.ts`, file: clientEntryFileTemplate(resourceName), standalone: true },
+  { name: `${resourceName}/atlas-resource.json`, file: atlasResourceJson(resourceName, isStandalone) },
+  { name: `${resourceName}/assets/resource.cfg`, file: cfgFromObject(gameResource), standalone: true }
 ]);
