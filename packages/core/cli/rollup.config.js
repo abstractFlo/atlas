59f9e963ec7f6399ba104bb50601e068f5eb8457
LeonardSSH <leonardssh22@gmail.com>
2021-08-09 18:48:21 +0200

    add a rollup global config

--- packages/core/cli/rollup.config.js
+++ packages/core/cli/rollup.config.js
@@ -1,11 +1,5 @@
-import ts from 'rollup-plugin-typescript2';
-import builtinModules from 'builtin-modules';
-import path from 'path';
-
+import baseConfig from '../../../scripts/rollup.config';
 import pkg from './package.json';
-import pkgRoot from '../../../package.json';
-
-let hasTSChecked = false;
 
 const outputConfigs = {
   cjs: {
@@ -14,50 +8,9 @@ const outputConfigs = {
   },
 };
 
-const allFormats = Object.keys(outputConfigs);
-const packageFormats = allFormats;
-const packageConfigs = packageFormats.map((format) => createConfig(outputConfigs[format]));
-
-export default packageConfigs;
-
-function createConfig(output) {
-  output.sourcemap = !!process.env.SOURCE_MAP;
-  output.externalLiveBindings = false;
-  output.banner = '#!/usr/bin/env node \n';
-
-  const shouldEmitDeclarations = !hasTSChecked;
-
-  const tsPlugin = ts({
-    check: !hasTSChecked,
-    tsconfig: path.resolve(__dirname, 'tsconfig.json'),
-    cacheRoot: path.resolve(__dirname, 'node_modules/.rts2_cache'),
-    tsconfigOverride: {
-      compilerOptions: {
-        sourceMap: output.sourcemap,
-        declaration: shouldEmitDeclarations,
-        declarationMap: shouldEmitDeclarations,
-      },
-    },
-  });
-
-  hasTSChecked = true;
-
-  const external = [
-    'yargs/helpers',
-    '@abstractflo/atlas-devtools',
-    ...builtinModules,
-    ...Object.keys(pkg.devDependencies || {}),
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-    ...Object.keys(pkgRoot.devDependencies || {}),
-    ...Object.keys(pkgRoot.dependencies || {}),
-    ...Object.keys(pkgRoot.peerDependencies || {}),
-  ];
-
-  return {
-    input: `src/atlas-cli.ts`,
-    external,
-    plugins: [tsPlugin],
-    output,
-  };
-}
+export default baseConfig({
+  input: 'src/atlas-cli.ts',
+  output: outputConfigs,
+  external: ['yargs/helpers', '@abstractflo/atlas-devtools'],
+  pkg,
+});
---
+++ scripts/rollup.config.js
@@ -0,0 +1,61 @@
+import ts from 'rollup-plugin-typescript2';
+import builtinModules from 'builtin-modules';
+import path from 'path';
+
+import pkgRoot from '../package.json';
+
+let hasTSChecked = false;
+
+const createRollupConfig = (options) => {
+  if (!Reflect.has(options, 'output')) options.output = {};
+
+  const allFormats = Object.keys(options.output);
+  const packageFormats = allFormats;
+  const packageConfigs = packageFormats.map((format) => createConfig(options, options.output[format]));
+
+  return packageConfigs;
+};
+
+function createConfig(options, output) {
+  if (!Reflect.has(options, 'input')) options.input = 'src/index.ts';
+  if (!Reflect.has(options, 'external')) options.external = [];
+  if (!Reflect.has(options, 'plugins')) options.plugins = [];
+  if (!Reflect.has(options, 'pkg')) options.pkg = {};
+
+  const { input, external, plugins, pkg } = options;
+
+  const shouldEmitDeclarations = !hasTSChecked;
+
+  const tsPlugin = ts({
+    check: !hasTSChecked,
+    tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+    cacheRoot: path.resolve(__dirname, 'node_modules/.rts2_cache'),
+    tsconfigOverride: {
+      compilerOptions: {
+        declaration: shouldEmitDeclarations,
+        declarationMap: shouldEmitDeclarations,
+      },
+    },
+  });
+
+  hasTSChecked = true;
+
+  external.push(
+    ...builtinModules,
+    ...Object.keys(pkg.devDependencies || {}),
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+    ...Object.keys(pkgRoot.devDependencies || {}),
+    ...Object.keys(pkgRoot.dependencies || {}),
+    ...Object.keys(pkgRoot.peerDependencies || {}),
+  );
+
+  return {
+    input,
+    external,
+    plugins: [tsPlugin, ...plugins],
+    output,
+  };
+}
+
+export default createRollupConfig;
