e322d1761570072950d5f45cd1326f87a7b54eb3
Narcis B <leonardssh22@gmail.com>
2021-08-09 13:09:30 -0700

    add a rollup global config (#22)
    Co-authored-by: Florian Uhlrich <f.uhlrich@abstractmedia.de>

--- stubs/package/rollup.config.js
+++ stubs/package/rollup.config.js
@@ -1,11 +1,5 @@
-import ts from 'rollup-plugin-typescript2';
-import builtinModules from 'builtin-modules';
-import path from 'path';
-
+import baseConfig from '../../scripts/rollup.config';
 import pkg from './package.json';
-import pkgRoot from '../../../package.json';
-
-let hasTSChecked = false;
 
 const outputConfigs = {
   esm: {
@@ -18,48 +12,8 @@ const outputConfigs = {
   },
 };
 
-const allFormats = Object.keys(outputConfigs);
-const packageFormats = allFormats;
-const packageConfigs = packageFormats.map((format) => createConfig(outputConfigs[format]));
-
-export default packageConfigs;
-
-function createConfig(output) {
-  output.sourcemap = !!process.env.SOURCE_MAP;
-  output.externalLiveBindings = false;
-
-  const shouldEmitDeclarations = !hasTSChecked;
-
-  const tsPlugin = ts({
-    check: !hasTSChecked,
-    tsconfig: path.resolve(__dirname, 'tsconfig.json'),
-    cacheRoot: path.resolve(__dirname, 'node_modules/.rts2_cache'),
-    tsconfigOverride: {
-      compilerOptions: {
-        sourceMap: output.sourcemap,
-        declaration: shouldEmitDeclarations,
-        declarationMap: shouldEmitDeclarations,
-      },
-    },
-  });
-
-  hasTSChecked = true;
-
-  const external = [
-    'alt-server',
-    ...builtinModules,
-    ...Object.keys(pkg.devDependencies || {}),
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-    ...Object.keys(pkgRoot.devDependencies || {}),
-    ...Object.keys(pkgRoot.dependencies || {}),
-    ...Object.keys(pkgRoot.peerDependencies || {}),
-  ];
-
-  return {
-    input: `src/index.ts`,
-    external,
-    plugins: [tsPlugin],
-    output,
-  };
-}
+export default baseConfig({
+  output: outputConfigs,
+  external: ['alt-shared', '@abstractflo/atlas-devtools', 'rxjs/operators'],
+  pkg,
+});
